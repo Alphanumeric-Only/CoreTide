117514826ed364818b88699527c5a384beae1a5a
vu-socprime <76435946+vu-socprime@users.noreply.github.com>
2025-05-12 18:28:16 +0300

    Update tide_schema.py

--- Engines/validation/tide_schema.py
+++ Engines/validation/tide_schema.py
@@ -30,7 +30,8 @@ def run():
 
         if schema in MODELS_INDEX:
             schema_data = JSONSCHEMAS_INDEX[schema]
-            schema_data['properties']['configurations']['additionalProperties'] = True
+            if 'configurations' in schema_data['properties']:
+                schema_data['properties']['configurations']['additionalProperties'] = True
             v = Draft7Validator(schema_data)
 
             for model in MODELS_INDEX[schema]:
